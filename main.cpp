12fb8cdc3613890bcdce61f0d9e4bda91c5240b6
mnj2kk <cryatbathroom@gmail.com>
2022-04-08 03:42:41 +0300

    updated misstakes fixing

--- main.cpp
+++ main.cpp
@@ -5,15 +5,15 @@
 
 const std::u16string u16_empty = convert_u16("");
 
-std::vector <std::vector <int> > source(256, std::vector <int> (256));
-
 // Допущение 3 ошибок в написании
 std::pair <std::u16string, int> find_with_corrects(InvertedSuggestion *ptr, const std::u16string &str,
-                                                   std::u16string &current_ans, char16_t p) {
+                                                   std::u16string &current_ans, char16_t p,
+                                                   std::vector <std::vector <int> > &source) {
     std::pair <std::u16string, int> u16_return(u16_empty, 4);
     if (p != '\0') {
         current_ans += p;
         size_t i = current_ans.size();
+        source[i][0] = (int) i;
         for (size_t j = 1; j <= str.size(); j++) {
             source[i][j] = std::min(source[i - 1][j] + 1,
                             std::min(source[i][j - 1] + 1, source[i - 1][j - 1] + (str[j - 1] != current_ans[i - 1])));
@@ -21,13 +21,9 @@ std::pair <std::u16string, int> find_with_corrects(InvertedSuggestion *ptr, cons
         if (source[i][str.size()] < u16_return.second) {
             u16_return = {current_ans, source[i][str.size()]};
         }
-        if (source[i][str.size()] > source[i - 1][str.size()]) {
-            current_ans.pop_back();
-            return {u16_empty, 4};
-        }
     }
     for (auto &[first, second] : ptr->next_link_) {
-        auto item = find_with_corrects(second, str, current_ans, first);
+        auto item = find_with_corrects(second, str, current_ans, first, source);
         if (u16_return.second > item.second) u16_return = item;
     }
     if (p != '\0')
@@ -36,8 +32,6 @@ std::pair <std::u16string, int> find_with_corrects(InvertedSuggestion *ptr, cons
 }
 
 int main(int argc, char *argv[]) {
-    for (size_t i = 0; i < 256; i++) source[i][0] = (int) i;
-    for (size_t j = 0; j < 256; j++) source[0][j] = (int) j;
     // Обработка отсутствия названия файла с логами
     if (argc < 2) {
         std::cerr << "Write path filename with <line_by_line_suggestion>" << std::endl;
@@ -69,9 +63,10 @@ int main(int argc, char *argv[]) {
             trim_string(word);
             if (word.empty()) continue;
             to_lower(word);
-            auto str = convert_u16(word);
-            std::u16string ans = u16_empty;
-            auto item = find_with_corrects(ptr, str, ans, '\0');
+            auto str = convert_u16(word), ans = u16_empty;
+            std::vector <std::vector <int> > source(DEPTH + 1, std::vector <int> (str.size() + 1));
+            for (size_t j = 0; j <= str.size(); j++) source[0][j] = (int) j;
+            auto item = find_with_corrects(ptr, str, ans, '\0', source);
             if (item.first != u16_empty) word = convert_u8(item.first);
             input.emplace_back(word, convert_u16(word), 0);
         }
